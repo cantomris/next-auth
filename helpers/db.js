a54b41cae1ec60366179cbdb1cf7d4e08a4db3da
Can Tomris <59895040+cantomris@users.noreply.github.com>
2021-08-05 09:26:58 +0300

    Update db.js

--- helpers/db.js
+++ helpers/db.js
@@ -1,7 +1,7 @@
 import { MongoClient } from 'mongodb'
 
 async function connectToDatabase() {
-  const client = await MongoClient.connect('mongodb+srv://cantomris:cantomris123@nextjsbackendcluster.kshwt.mongodb.net/auth-test?retryWrites=true&w=majority')
+  const client = await MongoClient.connect('mongodb+srv://<username>:<password>@nextjsbackendcluster.kshwt.mongodb.net/auth-test?retryWrites=true&w=majority')
 
   return client
 }
